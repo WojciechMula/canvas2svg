75308860e6a7c709dd55d09f61aa22eabc6b9f10
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-20 22:51:52 +0000

    minor
    git-svn-id: file:///mnt/data/SVNrepo@798 a13746d7-5020-4d46-8679-245a07f888a7

--- test/test-linecap.py
+++ test/test-linecap.py
@@ -5,7 +5,11 @@ print "line cap"
 capstyle = ["butt", "projecting", "round"]
 
 for i, style in enumerate(capstyle):
-	canv.create_line(50, 10+50*i, D-50, 10+50*i, width=10, capstyle=style)
+	canv.create_line(50, 10+50*i, D-50, 10+50*i, width=15, capstyle=style)
+	canv.create_line(50, 300+30*i, D/2, 150+30*i, D-50, 300+30*i, width=15, capstyle=style)
+	
+	canv.create_line(50, 10+50*i, D-50, 10+50*i, width=1.0, fill="white")
+	canv.create_line(50, 300+30*i, D/2, 150+30*i, D-50, 300+30*i, width=1.0, fill="white")
 
 thread.start_new_thread(test, (canv, __file__, True))
 root.mainloop()
