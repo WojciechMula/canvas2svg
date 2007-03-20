21f26ffe6ded9b637e9ee102aeb8515a872fcfdf
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-20 21:28:28 +0000

    implemented
    git-svn-id: file:///mnt/data/SVNrepo@793 a13746d7-5020-4d46-8679-245a07f888a7

---
+++ test/test-width.py
@@ -0,0 +1,12 @@
+from framework import *
+
+print "different line width"
+
+for i in xrange(1, D/20+1):
+	y = i*20
+	canv.create_line(10, y, 10+150, y, width=i/4.0)
+	canv.create_line(20+150, y, 20+300, y,
+		width=5, disabledwidth=i/4.0, state='disabled')
+
+thread.start_new_thread(test, (canv, __file__, True))
+root.mainloop()
