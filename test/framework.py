4e9ca9ac54b61f9a8ed909b864e5165cce587f96
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-20 21:12:17 +0000

    minor
    git-svn-id: file:///mnt/data/SVNrepo@791 a13746d7-5020-4d46-8679-245a07f888a7

--- test/framework.py
+++ test/framework.py
@@ -1,23 +1,29 @@
 from Tkconstants import *
 from random      import randint, seed, choice
-import Tkinter
+import Tkinter, os
 import canvasvg
+import thread
 
 D = 400
 def coord():
 	return randint(0, D)
 
-def dump(canv, name, pretty=False):
+def test(canv, name, pretty=False):
 	doc = canvasvg.SVGdocument()
 	canvasvg.convert(doc, canv)
 
-	x1, y1, x2, y2 = canv.bbox('all')
-	doc.documentElement.setAttribute('width',  str(x2))
-	doc.documentElement.setAttribute('height', str(y2))
+	doc.documentElement.setAttribute('width',  str(D))
+	doc.documentElement.setAttribute('height', str(D))
 
 	f = open(name + '.svg', 'w')
-	f.write(doc.toprettyxml())
+	if pretty:
+		f.write(doc.toprettyxml())
+	else:
+		f.write(doc.toxml())
 	f.close()
+	os.system("inkview %s.svg" % name)
+	raise SystemExit
+
 
 root = Tkinter.Tk()
 canv = Tkinter.Canvas(width=D, height=D, bg="#ffffff")
--- test/test-line.py
+++ test/test-line.py
@@ -1,11 +1,11 @@
 from framework import *
 
-
 n = 50
 print n, "lines (black, 1-pixel width)"
 
 for i in xrange(n):
 	canv.create_line(coord(), coord(), coord(), coord())
 
-dump(canv, __file__, True)
-#root.mainloop()
+
+thread.start_new_thread(test, (canv, __file__, True))
+root.mainloop()
