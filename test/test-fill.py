dd212d7fa1e3b8b591bfd954bfb9931388e95b3a
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-21 12:23:05 +0000

    implementation
    git-svn-id: file:///mnt/data/SVNrepo@813 a13746d7-5020-4d46-8679-245a07f888a7

---
+++ test/test-fill.py
@@ -0,0 +1,26 @@
+from framework import *
+
+n  = 10
+k1 = 3
+k2 = 15
+print "%d polygons with %d-%d vertices" % (n, k1, k2)
+
+for i in xrange(n):
+	k = randint(k1, k2)
+	p = []
+	for j in xrange(k):
+		p.append(coord())
+		p.append(coord())
+	item = canv.create_polygon(*p)
+	
+	color = random_color()
+	if randint(0, 1000) > 200:
+		canv.itemconfigure(item, fill=color, outline="black")
+	else:
+		canv.itemconfigure(item, fill=color, outline="black",
+			disabledfill="black", state=DISABLED)
+			
+
+thread.start_new_thread(test, (canv, __file__, True))
+root.mainloop()
+
