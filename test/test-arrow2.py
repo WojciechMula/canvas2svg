2f2706aee695dda0b801d4a449613559e08ec9bf
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-21 14:34:34 +0000

    implementation
    git-svn-id: file:///mnt/data/SVNrepo@816 a13746d7-5020-4d46-8679-245a07f888a7

--- test/test-arrow2.py
+++ test/test-arrow2.py
@@ -1,13 +1,17 @@
 from framework import *
 
-for i in xrange(50):
+k = 7
+for i in xrange(30):
 	d1 = randint(-10, 20)
 	d2 = randint(-10, 20)
 	d3 = randint(1, 20)
 	color = random_color()
-	canv.create_line(coord(), coord(), coord(), coord(),
+
+	points = [coord() for i in xrange(3*2, k*2)]
+	item = canv.create_line(*points)
+	canv.itemconfigure(item, 
 		fill		= random_color(),
-		arrow		= choice([LAST, BOTH, FIRST]),
+		arrow		=choice([LAST, BOTH, FIRST]),
 		arrowshape	= (d1, d2, d3))
 
 thread.start_new_thread(test, (canv, __file__, True))
