3e4501d389c825f7974ea726885a5a471be1f50b
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-21 18:05:06 +0000

    implementation
    git-svn-id: file:///mnt/data/SVNrepo@818 a13746d7-5020-4d46-8679-245a07f888a7

---
+++ test/test-text-anchor.py
@@ -0,0 +1,18 @@
+from framework import *
+
+anchors = [NW, N, NE, W, CENTER, E, SW, S, SE]
+
+for i, anchor in enumerate(anchors):
+	x = D/2 - ((i % 3)-1)*100
+	y = D/2 - ((i / 3)-1)*100
+
+	canv.create_oval(x-3, y-3, x+3, y+3, fill="black")
+	canv.create_text(x, y,
+		text	= anchor.upper(),
+		fill	= random_color(),
+		font	= ("Helvetica", -30),
+		anchor	= anchor,
+	)
+
+thread.start_new_thread(test, (canv, __file__, True))
+root.mainloop()
---
+++ test/test-text.py
@@ -0,0 +1,31 @@
+from framework import *
+
+texts = "Python Tkinter foo bar spam".split()
+
+for i in xrange(200):
+	x = coord()
+	y = coord()
+
+	def rand_weight():
+		if randint(0, 1000) > 500:
+			return "bold"
+		else:
+			return "normal"
+	
+	def rand_slant():
+		if randint(0, 1000) > 500:
+			return "italic"
+		else:
+			return "roman"
+
+	canv.create_text(x, y,
+		text = choice(texts),
+		fill = random_color(),
+		font = (choice(["Helvetica", "Times"]),
+			randint(10,30),
+			rand_weight(),
+			rand_slant()),
+	)
+
+thread.start_new_thread(test, (canv, __file__, True))
+root.mainloop()
