9b3eb77754675af864e9e54b1b9ed37fffac2500
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2008-11-08 16:41:01 +0000

    test for unicode texts
    git-svn-id: file:///mnt/data/SVNrepo@1733 a13746d7-5020-4d46-8679-245a07f888a7

---
+++ test/test-text-unicode.py
@@ -0,0 +1,38 @@
+# -*- encoding: iso-8859-2 -*-
+from framework import *
+root.title("Font family, size, slant & weight test ($Revision: 1.1 $)")
+
+texts = "Python Tkinter foo bar spam �곶濼".split()
+                                   # polish diacritical characters
+
+def tounicode(text):
+	return unicode(text, 'iso-8859-2').encode('utf-8')
+
+
+for i in xrange(200):
+	x = coord()
+	y = coord()
+
+	def rand_weight():
+		if randint(0, 1000) > 500:
+			return "bold"
+		else:
+			return "normal"
+	
+	def rand_slant():
+		if randint(0, 1000) > 500:
+			return "italic"
+		else:
+			return "roman"
+
+	canv.create_text(x, y,
+		text = choice(texts),
+		fill = random_color(),
+		font = (choice(["Helvetica", "Times"]),
+			randint(10,30),
+			rand_weight(),
+			rand_slant()),
+	)
+
+thread.start_new_thread(test, (canv, __file__, True, tounicode))
+root.mainloop()
