WojciechMula/canvas2svg

commit 0d6714906453274840adad726ca629a08d3f4f4c

Skip disabled items.
Disabled items have no bounding box, which leads to an error.
By skipping the disabled items the error cannot occur.

canvasvg.py
@@ -84,6 +84,8 @@ def convert(document, canvas, items=None, tounicode=None):
 			# left state unchanged
 			assert options['state'] in ['normal', DISABLED, 'hidden']
 
+                # skip hidden items
+                if options['state'] == 'hidden': continue
 
 		def get(name, default=""):
 			if state == ACTIVE and options.get(state + name):

commit 9223cd46824515a8409db9cf1eefe25ac75527a3

Fix: 0d6714906453274840adad726ca629a08d3f4f4c broke python3 compatibility

canvasvg.py
@@ -84,8 +84,8 @@ def convert(document, canvas, items=None, tounicode=None):
 			# left state unchanged
 			assert options['state'] in ['normal', DISABLED, 'hidden']
 
-                # skip hidden items
-                if options['state'] == 'hidden': continue
+		# skip hidden items
+		if options['state'] == 'hidden': continue
 
 		def get(name, default=""):
 			if state == ACTIVE and options.get(state + name):